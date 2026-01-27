dd349364d25be75c9b572e552280bb6a789c47be
jakobgotberg <jakob.gotberg@gmail.com>
2026-01-27 17:42:52 +0100

    added adjacency, degree, and laplacian matrices

--- exploration/matrix_utils.py
+++ exploration/matrix_utils.py
@@ -1,7 +1,6 @@
 import math
 import numpy as np
 import numpy.linalg as linalg
-
     
 def one(n):
     assert isinstance(n , int)
@@ -78,3 +77,42 @@ def _matrix_power_sum(B, start, end):
     for k in range(start, end):
         S += linalg.matrix_power(B, k)
     return S
+
+def get_n_from_E(E:list):
+    return max([e[1] for e in E])
+    
+def check_E(E):
+    assert isinstance(E, list)
+    assert all(isinstance(t, tuple) for t in E)
+    assert len(E) == len(set(E))
+    # assert 0 indexed
+    # assert no self-loops
+    # assert a < b in (a,b) in E
+    
+def laplacian_matrix(E:list):
+    return degree_matrix(E) - adjacency_matrix(E) 
+def degree_matrix(E:list):
+    '''
+    E is the list of undirected edges, represented as tuples
+    G is assumed to be connected
+    '''
+    check_E(E)
+    n = get_n_from_E(E)
+    D = [0] * n
+    for e in E:
+        D[e[0]] += 1
+        D[e[1]] += 1
+    return np.diag(D)
+    
+def adjacency_matrix(E:list):
+    '''
+    E is the list of undirected edges, represented as tuples
+    G is assumed to be connected
+    '''
+    check_E(E)
+    m = get_n_from_E(E)
+    A = np.zeros((n,n))
+    for e in E:
+        i, j = e
+        A[i][j] = A[j][i] = 1
+    return A
