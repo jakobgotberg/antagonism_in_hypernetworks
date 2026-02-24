cee7e338dcee0973fee5dd39957e2474396deddd
jakobgotberg <jakob.gotberg@gmail.com>
2026-02-24 12:27:01 +0100

    test looking good

--- hyper_algebraic_conflict.py
+++ hyper_algebraic_conflict.py
@@ -5,7 +5,40 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 
 
-def get_H(T):
+def assert_legal_A3(T):
+    '''
+    Iterate through all possible non-zero entreis in A3_i and assert that the same entries are covered in the relavent A3_j and A3_k, 
+    With the same sign
+    
+    There is no way to escape O(n^3). Each element should only be checked once: which is optimal.
+    As long as n is small, T can be in the L1 cache and the lookups are very fast desipte the strides.
+    '''
+
+    n = T.shape[0]
+    # A generator with unique triples: no two triples are permutations of each other.
+    g = ((i,j,k) for i,j,k in itertools.product(range(n), repeat=3) if i<j<k)
+
+    # len({...}) == 1: the length of the set of values of entires in A3 is 1 if all entries are equal.
+    lookup = lambda p : T[p[0]][p[1]][p[2]]
+    assert_edge = lambda permutations : len({lookup(p) for p in permutations}) == 1  
+
+    try:
+        while hyperedge:=next(g):
+            i,j,k = hyperedge
+            # calls assert_edge with a generator object: no wasted memory, only wasted CPU cycles
+            assert assert_edge( itertools.permutations(hyperedge) ), \
+                                        f"A3 is not legal for {hyperedge}:\
+                                          {T[i][j][k]},\
+                                          {T[i][k][j]},\
+                                          {T[j][i][k]},\
+                                          {T[j][k][i]},\
+                                          {T[k][i][j]},\
+                                          {T[k][j][i]}"
+
+    except StopIteration:
+        return
+
+def get_H(T, absolute=False):
     '''
     We define the degree matrix as: A2 + 1_3.T @ A3_1 + ... + 1_3.T @ A3_n
     No pairwise connection in this program, hence A2 is the zero matrix
@@ -13,61 +46,30 @@ def get_H(T):
     '''
     n = T.shape[0]
     _1 = mu.one(n)
-    A_tilde       =  np.zeros((n,n)) + np.array([(_1.T @ A3).ravel() for A3 in T[:]])
+    A_tilde       =  np.zeros((n,n)) + np.array([(_1.T @ A3).ravel() for A3 in T[:]]) if not absolute \
+                        else np.zeros((n,n)) + np.array([(_1.T @ abs(A3)).ravel() for A3 in T[:]])
     degree_matrix =  np.diag( (abs(A_tilde) @ _1).ravel() )
 
     # The normalized adjacency matrix, H
     return np.linalg.inv(degree_matrix) @ A_tilde
 
 
-def compute_balance(T, n, normalized=False):
-    def find_S_with_A(T,s):
-        '''
-        The hypergraph is balanced if:
-        Exists S : SA_iS <= 0 forall A_i
-
-        However, it seems only the 100% cooperative hypergraph has this property
-        '''
-        S = np.diag(s)
-        for ix, A in enumerate(T[:]):
-            if ((S @ A @ S) < 0).any():
-                return False
-            # All matricies are non-negative, the hypergraph is balanced
-            if ix == n-1:
-                return True
-
-    def find_S_with_H(T,D_inv,s):
-        pass
-        '''
-        No clue how the math works.
-        Should H be H from "Collective Descision making [...]" ? In that case, there should be no loop
-        '''
-
-        S = np.diag(s)
-        f_sum = 0
-        for A in T[:]:
-            H = D_inv @ A
-            f_sum += _1.T @ (abs(H) - (S @ H @ S)) @ _1
-        return f_sum
+def compute_fi(H):
+    '''
+    Find S through brute forcing all elements: s in {-1,1}^n
+    The returned value is normalized: not an integer. How can I convert it to one?
+    '''
+    n = H.shape[0]
+    _1 = mu.one(n)
+    fi = lambda S : (1/6) * _1.T @ (abs(H) - (S @ H @ S)) @ _1
+    return min([fi(np.diag(s)) for s in itertools.product([-1,1], repeat=n)]).ravel()
 
 
-    # find S through brute forcing all elements: s in {-1,1}^n
-    f_sum_min = sys.float_info.max
-    s_best = None
-    for s in itertools.product([-1,1], repeat=n):
-
-        _T = copy.deepcopy(T)
-        #f_sum = find_S_with_H(_T,D_inv,s) if normalized else find_S_with_A(_T,s)
-
-        if find_S_with_A(_T,s):
-            return True
-        del _T
-    #print(f"{antagonism:.1f} -> lowest frustration: {f_sum_min/2}")
-    return False
 
 def generate_hypergraphs(n, pr):
     '''
     Generate a list with an increasing amount of negative hyperedges.
+    Erdős–Rényi hypergraphs?
     'n' is the order
     'pr' is the edge probability.
     '''
@@ -75,14 +77,15 @@ def generate_hypergraphs(n, pr):
     assert n >= 3, "n too small"
     assert pr >= 0.0, "probability must be non-negative"
     r = range(n)
-    #pr = pr / n
+    pr = pr / n
     _1 = mu.one(n)
+    set_val = lambda T,p,val : T[p[0]][p[1]][p[2]] = val
 
     def set_edges(T,A,i):
-        '''
-        Set all permutations of the index (perm(3) = 6) to the value
-        Can it be done faster? 100% yes.
-        '''
+        r,c = np.nonzero(A)
+        g = ((j,k,val) for zip(r,c,A[r,c]))
+
+    # len({...}) == 1: the length of the set of values of entires in A3 is 1 if all entries are equal.
         for jk in itertools.product(r,r):
             j,k = jk
             if A[j][k] != 0:
@@ -91,7 +94,7 @@ def generate_hypergraphs(n, pr):
                     
 
     Ts = []
-    for antagonism in np.arange(0.0,1 +0.2, 0.2):
+    for antagonism in np.arange(0.0, 1+0.2, 0.2):
         T = np.zeros((n,n,n))
         for node in range(n):
             A = T[node]
@@ -103,10 +106,9 @@ def generate_hypergraphs(n, pr):
             # Zero out the i:th column and row, 
             A[node,:] = 0
             A[:,node] = 0
-            # Symmetry
-            #A = A + A.T
-            # No need to transpose, the 'set_edges' makes the matrix symmetric.
-            set_edges(T,A,node)
+            set_edges(T,node)
+
+        assert_legal_A3(T)
         Ts.append(T)
 
     return Ts
@@ -119,12 +121,15 @@ def main():
     p.add_argument("--nodes", type=int, default=12)
     p.add_argument("--prob", type=float, default=0.6)
     p.add_argument("--normal",action=argparse.BooleanOptionalAction, default=False)
+    p.add_argument("--abs",action=argparse.BooleanOptionalAction, default=False)
     a = p.parse_args()
 
     print("Since 'H' is the normalized adjacency, balance should mean \rho(H) = 1. Anything below 1 means unbalanced.")
     for T in generate_hypergraphs(a.nodes, a.prob):
-        algebraic_conflict = max(np.linalg.eigvals(get_H(T)))
-        print(f"{algebraic_conflict}")
+        H = get_H(T, a.abs)
+        algebraic_conflict = max(np.linalg.eigvals(H))
+        print(f"lambda_n: {algebraic_conflict}")
+        print(f"FI: {compute_fi(H)}\n")
 
 if __name__ == "__main__":
     t0 = time.perf_counter()
