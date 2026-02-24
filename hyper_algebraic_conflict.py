ad85e3ec523e6a89fb4367aa2ab890f750da995f
jakobgotberg <jakob.gotberg@gmail.com>
2026-02-24 14:35:01 +0100

    Perfection

--- hyper_algebraic_conflict.py
+++ hyper_algebraic_conflict.py
@@ -5,6 +5,9 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 
 
+f_str = lambda T,i,j,k : f"{T[i][j][k]}, {T[i][k][j]}, {T[j][i][k]}, {T[j][k][i]}, {T[k][i][j]}, {T[k][j][i]}\n"
+lookup = lambda T,p : T[p[0]][p[1]][p[2]]
+
 def assert_legal_A3(T):
     '''
     Iterate through all possible non-zero entreis in A3_i and assert that the same entries are covered in the relavent A3_j and A3_k, 
@@ -19,24 +22,20 @@ def assert_legal_A3(T):
     g = ((i,j,k) for i,j,k in itertools.product(range(n), repeat=3) if i<j<k)
 
     # len({...}) == 1: the length of the set of values of entires in A3 is 1 if all entries are equal.
-    lookup = lambda p : T[p[0]][p[1]][p[2]]
-    assert_edge = lambda permutations : len({lookup(p) for p in permutations}) == 1  
+    assert_edge = lambda permutations : len({lookup(T,p) for p in permutations}) == 1  
 
     try:
         while hyperedge:=next(g):
-            i,j,k = hyperedge
             # calls assert_edge with a generator object: no wasted memory, only wasted CPU cycles
             assert assert_edge( itertools.permutations(hyperedge) ), \
-                                        f"A3 is not legal for {hyperedge}:\
-                                          {T[i][j][k]},\
-                                          {T[i][k][j]},\
-                                          {T[j][i][k]},\
-                                          {T[j][k][i]},\
-                                          {T[k][i][j]},\
-                                          {T[k][j][i]}"
+                                        f"A3_{hyperedge[0]} is not legal for {hyperedge}: " + f_str(T, hyperedge[0], hyperedge[1], hyperedge[2])\
+                                        + str(T[hyperedge[0]]) + "\n\n" \
+                                        + str(T[hyperedge[1]]) + "\n\n" \
+                                        + str(T[hyperedge[2]]) + "\n\n"
 
     except StopIteration:
-        return
+        pass
+
 
 def get_H(T, absolute=False):
     '''
@@ -51,7 +50,7 @@ def get_H(T, absolute=False):
     degree_matrix =  np.diag( (abs(A_tilde) @ _1).ravel() )
 
     # The normalized adjacency matrix, H
-    return np.linalg.inv(degree_matrix) @ A_tilde
+    return np.linalg.inv(degree_matrix) @ A_tilde, A_tilde
 
 
 def compute_fi(H):
@@ -61,7 +60,7 @@ def compute_fi(H):
     '''
     n = H.shape[0]
     _1 = mu.one(n)
-    fi = lambda S : (1/6) * _1.T @ (abs(H) - (S @ H @ S)) @ _1
+    fi = lambda S : _1.T @ (abs(H) - (S @ H @ S)) @ _1
     return min([fi(np.diag(s)) for s in itertools.product([-1,1], repeat=n)]).ravel()
 
 
@@ -76,41 +75,56 @@ def generate_hypergraphs(n, pr):
 
     assert n >= 3, "n too small"
     assert pr >= 0.0, "probability must be non-negative"
+    Ts = []
     r = range(n)
     pr = pr / n
     _1 = mu.one(n)
-    set_val = lambda T,p,val : T[p[0]][p[1]][p[2]] = val
 
-    def set_edges(T,A,i):
-        r,c = np.nonzero(A)
-        g = ((j,k,val) for zip(r,c,A[r,c]))
+    def set_edges(T,C,i):
+        '''
+        Go through all new entires in C (upper triangle) and, for each edge, set the same values in the two related 'A' matricies.
+        Because of the mask, the function will not overwrite existing edges with a new signs, as far as I can tell.
+        Each entry _should_ only be written to once.
+        '''
+        negative_edges = 0
+        row, col = np.nonzero(C)
+        g = (edge for edge in zip(row, col, C[row, col]))
+        try:
+            while edge:=next(g):
+                j, k, val = edge
+                negative_edges += 1 if val == -1 else 0
+                T[i][j][k] = T[i][k][j] = T[j][i][k] = T[j][k][i] = T[k][i][j] = T[k][j][i] = val
+        except StopIteration:
+            return negative_edges
 
-    # len({...}) == 1: the length of the set of values of entires in A3 is 1 if all entries are equal.
-        for jk in itertools.product(r,r):
-            j,k = jk
-            if A[j][k] != 0:
-                for v in itertools.permutations([i,j,k]):
-                    T[v[0]][v[1]][v[2]] = A[j][k]
-                    
-
-    Ts = []
     for antagonism in np.arange(0.0, 1+0.2, 0.2):
         T = np.zeros((n,n,n))
         for node in range(n):
             A = T[node]
             # Iterate through all nodes, [0,n-1], assign each a signed random upper triangle.
             B = np.triu((np.random.rand(n,n) < pr).astype(np.int8), k=1) * np.sign(np.triu(np.random.rand(n,n) - antagonism)).astype(np.int8)
+            # Zero out the i:th column and row, 
+            B[node,:] = 0
+            B[:,node] = 0
+
             # This ensures that entries made in previous iterations are not overwritten.
             mask = (A == 0) & (B != 0)
-            A[mask] = B[mask]
-            # Zero out the i:th column and row, 
-            A[node,:] = 0
-            A[:,node] = 0
-            set_edges(T,node)
+            if (mask == False).all():
+                # No new edges added in this iteration.
+                continue
+
+            # C holds the new, legal edges generated with B in its non-zero entries
+            C = np.zeros((n,n))
+            C[mask] = B[mask]
+            set_edges(T, C, node)
+            #A[mask] = B[mask]
+            #A += A.T
+            del B,C
 
         assert_legal_A3(T)
         Ts.append(T)
 
+    assert np.isin(np.unique(Ts), [-1,0,1]).all(), f"Ts contains: {np.unique(Ts)}"
     return Ts
 
     
@@ -124,12 +138,16 @@ def main():
     p.add_argument("--abs",action=argparse.BooleanOptionalAction, default=False)
     a = p.parse_args()
 
-    print("Since 'H' is the normalized adjacency, balance should mean \rho(H) = 1. Anything below 1 means unbalanced.")
     for T in generate_hypergraphs(a.nodes, a.prob):
-        H = get_H(T, a.abs)
+        E = np.count_nonzero(T)
+        E_n = np.count_nonzero(T == -1)
+        H, A_tilde = get_H(T, a.abs)
         algebraic_conflict = max(np.linalg.eigvals(H))
+        print(f"Edges: {E}\t Negative Edges: {E_n}")
         print(f"lambda_n: {algebraic_conflict}")
-        print(f"FI: {compute_fi(H)}\n")
+        print(f"FI: {compute_fi(H)}")
+        print(f"FI (A_tilde): {compute_fi(A_tilde)}")
+        print()
 
 if __name__ == "__main__":
     t0 = time.perf_counter()
